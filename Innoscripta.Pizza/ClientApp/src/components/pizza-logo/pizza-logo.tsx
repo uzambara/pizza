1d5f313886c8d982441073e7118d8eb02c97d157
egor simonov <egor.simonov.wz@gmail.com>
2020-05-23 22:05:30 +0400

    Added pizza-logo

---
+++ Innoscripta.Pizza/ClientApp/src/components/pizza-logo/pizza-logo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Images} from "../../images";
+import * as styles from "./pizza-logo.scss";
+
+export function PizzaLogo() {
+    return <a className={styles.pizzaLogo}>
+        <img className={styles.pizzaLogoImg} src={Images.Shared.logo}/>
+        <h1 className={styles.pizzaLogoTitle}>Eat me</h1>
+    </a>
+}
