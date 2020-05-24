c3e84f85648a35c5d97a711e3c5359cd8077c404
egor simonov <egor.simonov.wz@gmail.com>
2020-05-24 21:15:59 +0400

    Added cart item factory

---
+++ Innoscripta.Pizza/ClientApp/src/factories/cart-item.factory.ts
@@ -0,0 +1,17 @@
+﻿import {IProductModelRemote} from "../models-remote";
+import {ICartItem} from "../models";
+
+function createCartItem(product: IProductModelRemote): ICartItem {
+    return {
+        count: 1,
+        imageUrl: product.img,
+        name: product.name,
+        price: product.price,
+        productId: product.id,
+        productType: product.type
+    }
+}
+
+export const cartItemFactory = {
+    createCartItem
+};
