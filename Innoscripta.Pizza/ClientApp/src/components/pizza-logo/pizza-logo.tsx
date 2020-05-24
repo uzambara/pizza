ba9d4b91a639c4c6c32248831154dd30cbcc1cb1
egor simonov <egor.simonov.wz@gmail.com>
2020-05-24 21:03:04 +0400

    Added alt to logo image

--- Innoscripta.Pizza/ClientApp/src/components/pizza-logo/pizza-logo.tsx
+++ Innoscripta.Pizza/ClientApp/src/components/pizza-logo/pizza-logo.tsx
@@ -4,7 +4,7 @@ import * as styles from "./pizza-logo.scss";
 
 export function PizzaLogo() {
     return <a className={styles.pizzaLogo}>
-        <img className={styles.pizzaLogoImg} src={Images.Shared.logo}/>
+        <img className={styles.pizzaLogoImg} src={Images.Shared.logo} alt="pizza"/>
         <h1 className={styles.pizzaLogoTitle}>Eat me</h1>
     </a>
 }
