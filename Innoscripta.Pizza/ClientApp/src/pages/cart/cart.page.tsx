3d8e425f99cc4dca6f5307c2b2b909304b92d13e
egor simonov <egor.simonov.wz@gmail.com>
2020-05-24 21:15:19 +0400

    Added cart page component

---
+++ Innoscripta.Pizza/ClientApp/src/pages/cart/cart.page.tsx
@@ -0,0 +1,24 @@
+﻿import React, {memo} from "react";
+import * as styles from "./cart.page.scss";
+import {IPageProps} from "../page-props";
+import {withPageWrapper} from "../../hocs";
+import {useSelector} from "react-redux";
+import {cartSelectors} from "../../redux/selectors";
+import {PizzaCartItem} from "../../components/pizza-cart-item/pizza-cart-item";
+
+export interface ICartPageProps extends IPageProps {
+}
+
+function CartPageComponent(props: ICartPageProps) {
+    const {} = props;
+    const cart = useSelector(cartSelectors.selectCart);
+    return <>
+        <h2 className={styles.title}>Your order</h2>
+        {
+            Object.keys(cart.items).map((productId) =>
+                <PizzaCartItem key={productId} cartItem={cart.items[productId]}/>)
+        }
+    </>
+}
+
+export const CartPage = memo(withPageWrapper<ICartPageProps>(CartPageComponent));
