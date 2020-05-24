bdf44c2aea78c248d202780b73a3c0347ec7e7eb
egor simonov <egor.simonov.wz@gmail.com>
2020-05-24 21:12:01 +0400

    Added button control component

---
+++ Innoscripta.Pizza/ClientApp/src/components/controls/pizza-button/pizza-button.tsx
@@ -0,0 +1,27 @@
+﻿import React, {memo, PropsWithChildren} from "react";
+import cn from "classnames";
+import * as styles from "./pizza-button.scss";
+
+
+export interface IPizzaButtonProps {
+    onClick: () => void,
+    className?: string,
+    type: "submit" | "alert" | "success" | "simple"
+}
+
+function PizzaButtonComponent(props: PropsWithChildren<IPizzaButtonProps>) {
+    const {onClick, children, className, type} = props;
+
+    return <button
+        onClick={onClick}
+        className={cn(className, styles.button, {
+            [styles.buttonAlert]: type === "alert",
+            [styles.buttonSubmit]: type === "submit",
+            [styles.buttonSuccess]: type === "success",
+        })}
+    >
+        {children}
+    </button>
+}
+
+export const PizzaButton = memo(PizzaButtonComponent);
