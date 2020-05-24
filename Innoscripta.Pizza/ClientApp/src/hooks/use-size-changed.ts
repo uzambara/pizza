955d224ad9aeaf6e9a4d5f69b9fc1f7f0dc11db5
egor simonov <egor.simonov.wz@gmail.com>
2020-05-24 21:00:46 +0400

    Added use size changed hook

---
+++ Innoscripta.Pizza/ClientApp/src/hooks/use-size-changed.ts
@@ -0,0 +1,44 @@
+﻿import {useCallback, useLayoutEffect, useRef, useState} from "react";
+import ResizeObserver from 'resize-observer-polyfill';
+import {throttle} from "throttle-debounce";
+
+export function useSizeChanged() {
+    const ref = useRef<HTMLElement>();
+    const [size, setSize] = useState({height: 0, width: 0});
+
+    const handleResize = useCallback(throttle(500, () => {
+        if (ref.current) {
+            const newSize = getSize(ref.current);
+            setSize(newSize);
+        }
+    }), [ref.current]);
+
+    useLayoutEffect(() => {
+        if (!ref.current)
+            return;
+
+        const resizeObserver = new ResizeObserver(handleResize);
+        resizeObserver.observe(ref.current);
+
+        return () => resizeObserver.disconnect();
+    }, [ref.current]);
+
+    return {
+        ref,
+        size
+    }
+}
+
+function getSize(el) {
+    if (!el) {
+        return {
+            width: 0,
+            height: 0
+        }
+    }
+
+    return {
+        width: el.offsetWidth,
+        height: el.offsetHeight
+    }
+}
