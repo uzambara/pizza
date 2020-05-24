ef6c54b7659347b6a77b647ee5d53bd50aaa200d
egor simonov <egor.simonov.wz@gmail.com>
2020-05-24 21:02:18 +0400

    Added product remote model

---
+++ Innoscripta.Pizza/ClientApp/src/models-remote/product/product.ts
@@ -0,0 +1,10 @@
+﻿import {ProductType} from "../../enums";
+
+export interface IProductModelRemote {
+    id: number,
+    name: string,
+    price: number,
+    img: string,
+    description: string,
+    type: ProductType
+}
