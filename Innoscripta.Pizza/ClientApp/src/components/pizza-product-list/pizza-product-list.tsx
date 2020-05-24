68f8fa3a714f5a8261c3a88551b0fc5aa2cc0636
egor simonov <egor.simonov.wz@gmail.com>
2020-05-24 21:11:43 +0400

    Added product list component

---
+++ Innoscripta.Pizza/ClientApp/src/components/pizza-product-list/pizza-product-list.tsx
@@ -0,0 +1,27 @@
+﻿import React, {memo} from "react";
+import {PizzaProductCard} from "..";
+import {IProductModelRemote} from "../../models-remote";
+import * as styles from "./pizza-product-list.scss";
+import cn from "classnames";
+import {useSelector} from "react-redux";
+import {settingsSelectors} from "../../redux/selectors";
+
+
+
+export interface IPizzaProductListProps {
+    items: IProductModelRemote[],
+    className?: string
+}
+
+function PizzaProductListComponent(props: IPizzaProductListProps) {
+    const {items, className} = props;
+    const currencyType = useSelector(settingsSelectors.selectCurrencyType);
+    return <ul className={cn(styles.list, className)}>
+        {
+            items.map((product, idx) =>
+                <li className={styles.listItem} key={idx}><PizzaProductCard currencyType={currencyType} productModel={product}/></li>)
+        }
+    </ul>
+}
+
+export const PizzaProductList = memo(PizzaProductListComponent);
