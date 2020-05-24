882fad532dfc663f8b4f19ef2aa3da3a862aaa98
egor simonov <egor.simonov.wz@gmail.com>
2020-05-24 21:02:39 +0400

    Added select control

---
+++ Innoscripta.Pizza/ClientApp/src/components/controls/pizza-select/pizza-select.tsx
@@ -0,0 +1,30 @@
+﻿import React, {ChangeEventHandler, memo} from "react";
+import * as styles from "./pizza-select.scss";
+import cn from "classnames";
+
+export interface ISelectItem {
+    value: string | number,
+    text: string | number
+}
+
+export interface IPizzaSelectProps {
+    items: ISelectItem[],
+    onChange: (value: string | number) => void,
+    value: string | number,
+     className?: string
+}
+
+function PizzaSelectComponent(props: IPizzaSelectProps) {
+    const {items, onChange, value, className} = props;
+    const onChangeInternal: ChangeEventHandler<HTMLSelectElement> = (ev) => onChange(ev.target.value);
+
+    return <select className={cn(styles.select, className)} onChange={onChangeInternal} value={value}>
+        {
+            items.map(item => <option key={item.value} value={item.value}>
+                {item.text}
+            </option>)
+        }
+    </select>
+}
+
+export const PizzaSelect = memo(PizzaSelectComponent);
