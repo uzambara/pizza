fcc8ab22a9e69ba437316a3b9217776a3e62883f
egor simonov <egor.simonov.wz@gmail.com>
2020-05-24 20:59:45 +0400

    Added with-page-wrapper HOC

---
+++ Innoscripta.Pizza/ClientApp/src/hocs/with-page-wrapper.tsx
@@ -0,0 +1,13 @@
+﻿import React, {FunctionComponent} from "react";
+import {IPageProps} from "../pages";
+
+
+
+export function withPageWrapper<T extends IPageProps>(Component: any): FunctionComponent<T> {
+    return function(props: T) {
+        const {className, paddingTop, ...other} = props;
+        return <main className={className} style={{paddingTop: paddingTop}}>
+            <Component {...other}/>
+        </main>
+    }
+}
