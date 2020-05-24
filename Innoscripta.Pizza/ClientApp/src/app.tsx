f473b2663d6d49f0387f84702083209faf5a7a41
egor simonov <egor.simonov.wz@gmail.com>
2020-05-24 22:17:55 +0400

    Added padding top of main content dependent on header height

--- Innoscripta.Pizza/ClientApp/src/app.tsx
+++ Innoscripta.Pizza/ClientApp/src/app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Route, Switch} from "react-router";
 import {Routing} from "./routing";
 import {MainPage} from "./pages";
@@ -6,11 +6,12 @@ import {PizzaHeader} from "./components";
 import * as styles from "./app.scss";
 
 export function App() {
+    const [height, setHeightSize] = useState(0);
     return <>
-        <PizzaHeader className={styles.header}/>
+        <PizzaHeader className={styles.header} onHeightChanged={setHeightSize}/>
         <Switch>
             <Route exact path={Routing.MainPage}>
-                <MainPage className={styles.mainContainer}/>
+                <MainPage className={styles.mainContainer} paddingTop={height}/>
             </Route>
         </Switch>
     </>
