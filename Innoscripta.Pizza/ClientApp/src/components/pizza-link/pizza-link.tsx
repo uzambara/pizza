e97a5505f7c880176abae6246c8529ae5fd635a5
egor simonov <egor.simonov.wz@gmail.com>
2020-05-24 21:05:08 +0400

    Wrap pizza link component in memo

--- Innoscripta.Pizza/ClientApp/src/components/pizza-link/pizza-link.tsx
+++ Innoscripta.Pizza/ClientApp/src/components/pizza-link/pizza-link.tsx
@@ -1,4 +1,4 @@
-﻿import React, {PropsWithChildren} from "react";
+﻿import React, {memo, PropsWithChildren} from "react";
 import {Link} from "react-router-dom";
 
 export interface IPizzaLinkProps {
@@ -6,7 +6,9 @@ export interface IPizzaLinkProps {
     className?: string
 }
 
-export function PizzaLink(props: PropsWithChildren<IPizzaLinkProps>) {
+function PizzaLinkComponent(props: PropsWithChildren<IPizzaLinkProps>) {
     const {children, href, className} = props;
     return <Link to={href} className={className}>{children}</Link>
-};
+}
+
+export const PizzaLink = memo(PizzaLinkComponent);
