99f185aeed77b9c087f862ee15e8be8e7127b5d0
egor simonov <egor.simonov.wz@gmail.com>
2020-05-24 21:06:25 +0400

    Added cart component

---
+++ Innoscripta.Pizza/ClientApp/src/components/pizza-cart/pizza-cart.tsx
@@ -0,0 +1,26 @@
+﻿import React, {memo} from "react";
+import {CurrencyType} from "../../enums";
+import {currencyUtils} from "../../utils";
+import * as styles from "./pizza-cart.scss";
+import {Images} from "../../images";
+import cn from "classnames";
+import {PizzaLink} from "..";
+import {Routing} from "../../routing";
+
+export interface IPizzaCartProps {
+    totalPrice: number,
+    currencyType: CurrencyType,
+    className?: string
+}
+
+function PizzaCartComponent(props: IPizzaCartProps) {
+    const {totalPrice, currencyType, className} = props;
+
+    const text = `${currencyUtils.getPriceString(totalPrice, currencyType)}`;
+    return <PizzaLink className={cn(styles.cart, className)} href={Routing.Cart}>
+        <img src={Images.Shared.cart} alt="cart" className={styles.cartImage}/>
+        <span className={styles.cartText} dangerouslySetInnerHTML={{__html: text}}/>
+    </PizzaLink>
+}
+
+export const PizzaCart = memo(PizzaCartComponent);
