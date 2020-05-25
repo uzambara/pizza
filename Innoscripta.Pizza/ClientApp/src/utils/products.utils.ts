b69d833c7f1ed6502b5bacd27340026c6093f07c
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 02:33:56 +0400

    Added product utils

---
+++ Innoscripta.Pizza/ClientApp/src/utils/products.utils.ts
@@ -0,0 +1,12 @@
+﻿import memoize from "memoizee";
+import {IProductModelRemote} from "../models-remote";
+import {ProductType} from "../enums";
+
+const getProductsByType = memoize((products: IProductModelRemote[], type: ProductType) => {
+    return products.filter(product => product.type == type)
+});
+
+
+export const productUtils = {
+    getProductsByType
+};
