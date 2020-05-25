42e9508a31d906eff394299893769484c3b4b16f
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 00:38:31 +0400

    Added use counter hook

---
+++ Innoscripta.Pizza/ClientApp/src/hooks/use-counter.ts
@@ -0,0 +1,18 @@
+﻿import {useCallback, useState} from "react";
+
+export function useCounter(initialValue: number) {
+    const [counterValue, setCounterValue] = useState(initialValue);
+
+    const onIncrease = useCallback(() => {
+        setCounterValue(prev => prev + 1);
+    }, []);
+    const onDecrease = useCallback(() => {
+        setCounterValue(prev => prev - 1);
+    }, []);
+
+    return {
+        counterValue,
+        onIncrease,
+        onDecrease
+    }
+}
