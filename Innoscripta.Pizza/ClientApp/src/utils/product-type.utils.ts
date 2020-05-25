70d9efd369eddfecbfd3b361270eda1e198ddb1d
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 00:38:20 +0400

    Added product type utils

---
+++ Innoscripta.Pizza/ClientApp/src/utils/product-type.utils.ts
@@ -0,0 +1,18 @@
+﻿import {ProductType} from "../enums";
+import {Images} from "../images";
+
+function getProductTypeImage(productType: ProductType): string {
+    switch (productType) {
+        case ProductType.Drink:
+            return Images.ProductType.drinks;
+        case ProductType.Pizza:
+        default:
+            return Images.ProductType.pizza;
+    }
+}
+
+
+
+export const productTypeUtils = {
+    getProductTypeImage
+};
