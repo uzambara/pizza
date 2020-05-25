89bd4cf8d52af7ea59e90ba0d09070b9b3ede15c
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 00:26:35 +0400

    Added cart utils

---
+++ Innoscripta.Pizza/ClientApp/src/utils/cart.utils.ts
@@ -0,0 +1,18 @@
+﻿import memoize from "memoizee"
+import {CartItemsMap} from "../redux/reducers";
+
+
+const calcTotalPrice = memoize((cart: CartItemsMap): number => {
+    console.log("calc");
+    let result = 0;
+    for(let id in cart) {
+        const cartItem = cart[id];
+        result += cartItem.count * cartItem.price;
+    }
+
+    return result;
+});
+
+export const cartUtils = {
+    calcTotalPrice
+};
