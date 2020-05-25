b1e40c8f1c0733bd6f087afe3061053b1a1ad017
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 01:15:45 +0400

    Added local storage service.

---
+++ Innoscripta.Pizza/ClientApp/src/services/local-storage.service.ts
@@ -0,0 +1,13 @@
+export class LocalStorageService {
+    public static getItem<T extends object>(key: string): T {
+        const data = localStorage.getItem(key);
+
+        if(data != null) {
+            return JSON.parse(data);
+        }
+    }
+
+    public static setItem<T extends object>(key: string, object: T): void {
+        localStorage.setItem(key, JSON.stringify(object));
+    }
+}
