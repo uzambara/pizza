9a58607cbea000e186517764e9745963c6102010
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 00:35:13 +0400

    Added cart page and total price selector from state.

--- Innoscripta.Pizza/ClientApp/src/app.tsx
+++ Innoscripta.Pizza/ClientApp/src/app.tsx
@@ -1,17 +1,23 @@
 import React, {useState} from "react";
 import {Route, Switch} from "react-router";
 import {Routing} from "./routing";
-import {MainPage} from "./pages";
+import {CartPage, MainPage} from "./pages";
 import {PizzaHeader} from "./components";
 import * as styles from "./app.scss";
+import {useSelector} from "react-redux";
+import {cartSelectors} from "./redux/selectors";
 
 export function App() {
     const [height, setHeightSize] = useState(0);
+    const totalPrice = useSelector(cartSelectors.selectTotalPrice);
     return <>
-        <PizzaHeader className={styles.header} onHeightChanged={setHeightSize}/>
+        <PizzaHeader className={styles.header} onHeightChanged={setHeightSize} totalPrice={totalPrice}/>
         <Switch>
             <Route exact path={Routing.MainPage}>
                 <MainPage className={styles.mainContainer} paddingTop={height}/>
+            </Route>
+            <Route exact path={Routing.Cart}>
+                <CartPage className={styles.mainContainer} paddingTop={height} totalPrice={totalPrice}/>
             </Route>
         </Switch>
     </>
