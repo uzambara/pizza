d996549962a888e499c78e88e866114574556435
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 00:28:28 +0400

    Made all product cards the same height

--- Innoscripta.Pizza/ClientApp/src/pages/cart/cart.page.tsx
+++ Innoscripta.Pizza/ClientApp/src/pages/cart/cart.page.tsx
@@ -3,21 +3,20 @@ import * as styles from "./cart.page.scss";
 import {IPageProps} from "../page-props";
 import {withPageWrapper} from "../../hocs";
 import {useSelector} from "react-redux";
-import {cartSelectors} from "../../redux/selectors";
-import {PizzaCartItem} from "../../components";
+import {cartSelectors, settingsSelectors} from "../../redux/selectors";
+import {PizzaCartList} from "../../components";
 
 export interface ICartPageProps extends IPageProps {
+    totalPrice: number
 }
 
 function CartPageComponent(props: ICartPageProps) {
-    const {} = props;
-    const cart = useSelector(cartSelectors.selectCart);
+    const {totalPrice} = props;
+    const cartItemsMap = useSelector(cartSelectors.selectCartItemsMap);
+    const currencyType = useSelector(settingsSelectors.selectCurrencyType);
     return <>
         <h2 className={styles.title}>Your order</h2>
-        {
-            Object.keys(cart.items).map((productId) =>
-                <PizzaCartItem key={productId} cartItem={cart.items[productId]}/>)
-        }
+        <PizzaCartList totalPrice={totalPrice} currencyType={currencyType} cartItemsMap={cartItemsMap}/>
     </>
 }
 
