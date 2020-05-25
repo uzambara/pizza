e4ea47dba90216c774b43be168ffb5d7286db0c0
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 00:26:07 +0400

    Added pizza cart item component

---
+++ Innoscripta.Pizza/ClientApp/src/components/pizza-cart-item/pizza-cart-item.tsx
@@ -0,0 +1,40 @@
+﻿import React, {memo, useCallback, useEffect} from "react";
+import {ICartItem} from "../../models";
+import * as styles from "./pizza-cart-item.scss";
+import {PizzaCounter} from "../pizza-counter/pizza-counter";
+import {useCounter} from "../../hooks";
+import {currencyUtils} from "../../utils";
+import {CurrencyType} from "../../enums";
+import {useDispatch} from "react-redux";
+import {cartActions} from "../../redux/actions";
+
+export interface IPizzaCartItem {
+    cartItem: ICartItem,
+    currencyType: CurrencyType
+}
+
+function PizzaCartItemComponent(props: IPizzaCartItem) {
+    const {cartItem, currencyType} = props;
+    const {imageUrl, price, count, productId, name} = cartItem;
+    const dispatch = useDispatch();
+    const deleteItem = () => dispatch(cartActions.RemoveItem(productId));
+
+    const onIncreaseCount = useCallback(() => {
+        dispatch(cartActions.ChangeItemCount(productId, 1));
+    }, []);
+    const onDecreaseCount = useCallback(() => {
+        dispatch(cartActions.ChangeItemCount(productId, -1));
+    }, []);
+
+    return <div className={styles.wrapper}>
+        <img src={imageUrl} alt="product" className={styles.productImage}/>
+        <div className={styles.counterAndNameWrapper}>
+            <PizzaCounter value={count} onIncrease={onIncreaseCount} onDecrease={onDecreaseCount} className={styles.counter}/>
+            <h3 className={styles.productName}>{name}</h3>
+        </div>
+        <span className={styles.price} dangerouslySetInnerHTML={{__html: currencyUtils.getPriceString(price, currencyType)}}/>
+        <button className={styles.deleteButton} onClick={deleteItem}>✖</button>
+    </div>
+}
+
+export const PizzaCartItem = memo(PizzaCartItemComponent);
