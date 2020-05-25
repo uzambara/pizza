6b2085d946be5fc5a1d98fbf119ca71055675f03
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 00:25:53 +0400

    Added pizza counter component

---
+++ Innoscripta.Pizza/ClientApp/src/components/pizza-counter/pizza-counter.tsx
@@ -0,0 +1,24 @@
+﻿import {memo} from "react";
+import {PizzaButton} from "..";
+import React from "react";
+import cn from "classnames";
+import * as styles from "./pizza-counter.scss";
+
+interface IPizzaCounterProps {
+    value,
+    onIncrease: () => void,
+    onDecrease: () => void,
+    className?:  string
+}
+
+function PizzaCounterComponent(props: IPizzaCounterProps) {
+    const {value, onIncrease, onDecrease, className} = props;
+
+    return <div className={cn(styles.wrapper, className)}>
+        <PizzaButton onClick={onIncrease} type={"submit"} className={styles.button}>+</PizzaButton>
+        <span className={styles.counterValue}>{value}</span>
+        <PizzaButton onClick={onDecrease} type={"submit"} className={styles.button}>–</PizzaButton>
+    </div>
+}
+
+export const PizzaCounter = memo(PizzaCounterComponent);
