0e4b78b68be3ad7336ea1529670b1277840d7a4c
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 00:27:01 +0400

    Added RemoveItem, ChangeItemCount actions

--- Innoscripta.Pizza/ClientApp/src/redux/actions/cart.actions.ts
+++ Innoscripta.Pizza/ClientApp/src/redux/actions/cart.actions.ts
@@ -1,10 +1,14 @@
 ﻿import {ICartItem} from "../../models";
 
 export type CartAction =
-    IAddToCart;
+    IAddToCart
+    | IRemoveItem
+    | IChangeItemCount;
 
 export enum CartActionType {
-    AddToCart= "ADD_TO_CART"
+    AddToCart = "ADD_TO_CART",
+    RemoveItem = "REMOVE_ITEM",
+    ChangeItemCount = "CHANGE_ITEM_COUNT",
 }
 
 export interface IAddToCart {
@@ -19,6 +23,38 @@ function AddToCart(item: ICartItem): IAddToCart {
     }
 }
 
+export interface IRemoveItem {
+    type: CartActionType.RemoveItem,
+    payload: number
+}
+
+export function RemoveItem(id: number): IRemoveItem {
+    return {
+        type: CartActionType.RemoveItem,
+        payload: id
+    }
+}
+
+export interface IChangeItemCount {
+    type: CartActionType.ChangeItemCount,
+    payload: {
+        id: number,
+        diff: number
+    }
+}
+
+export function ChangeItemCount(id: number, diff: number): IChangeItemCount {
+    return {
+        type: CartActionType.ChangeItemCount,
+        payload: {
+            id,
+            diff
+        }
+    }
+}
+
 export const cartActions = {
-    AddToCart
+    AddToCart,
+    RemoveItem,
+    ChangeItemCount
 };
