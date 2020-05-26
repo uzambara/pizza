aa2d618eadd7f363bd45faa918eec9cf8b3f9a8f
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 20:49:44 +0400

    Added redux form reducer

--- Innoscripta.Pizza/ClientApp/src/redux/reducers/index.ts
+++ Innoscripta.Pizza/ClientApp/src/redux/reducers/index.ts
@@ -2,17 +2,20 @@
 import {cartReducer, ICartState, CartItemsMap} from "./cart.reducer";
 import {ISettingsState, settingsReducer} from "./settings.reducer";
 import {IProductsState, productsReducer} from "./products.reducer";
+import { reducer as formReducer } from 'redux-form';
 
 export interface IGlobalState {
     cart: ICartState,
     settings: ISettingsState,
-    products: IProductsState
+    products: IProductsState,
+    form: any
 }
 
 export const rootReducer = combineReducers<IGlobalState>({
     cart: cartReducer,
     settings: settingsReducer,
-    products: productsReducer
+    products: productsReducer,
+    form: formReducer
 });
 
 export {
