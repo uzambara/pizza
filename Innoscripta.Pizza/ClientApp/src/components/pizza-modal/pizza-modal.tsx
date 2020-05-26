1f44e7457e3dd8520abb15374de763026de0f361
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 20:48:43 +0400

    Added modal

---
+++ Innoscripta.Pizza/ClientApp/src/components/pizza-modal/pizza-modal.tsx
@@ -0,0 +1,19 @@
+﻿import React, {memo, PropsWithChildren} from "react";
+import cn from "classnames";
+import * as styles from "./pizza-modal.scss";
+
+export interface IPizzaModalProps {
+    classNameContainer?: string,
+    classNameModal?: string
+}
+
+function PizzaModalComponent(props: PropsWithChildren<IPizzaModalProps>) {
+    const {children, classNameContainer, classNameModal} = props;
+    return <div className={cn(styles.wrapper, classNameContainer)}>
+        <div className={cn(styles.modal, classNameModal)}>
+            {children}
+        </div>
+    </div>
+}
+
+export const PizzaModal = memo(PizzaModalComponent);
