5bfd625110bb187d8eb7271ca51ddcf560480e6e
egor simonov <egor.simonov.wz@gmail.com>
2020-05-26 20:50:27 +0400

    Added order submit form

--- Innoscripta.Pizza/ClientApp/src/pages/cart/cart.page.tsx
+++ Innoscripta.Pizza/ClientApp/src/pages/cart/cart.page.tsx
@@ -1,10 +1,10 @@
-﻿import React, {memo} from "react";
+﻿import React, {memo, useState} from "react";
 import * as styles from "./cart.page.scss";
 import {IPageProps} from "../page-props";
 import {withPageWrapper} from "../../hocs";
 import {useSelector} from "react-redux";
 import {cartSelectors, settingsSelectors} from "../../redux/selectors";
-import {PizzaCartList} from "../../components";
+import {PizzaCartList, PizzaModal, PizzaOrderForm} from "../../components";
 
 export interface ICartPageProps extends IPageProps {
     totalPrice: number
@@ -14,7 +14,14 @@ function CartPageComponent(props: ICartPageProps) {
     const {totalPrice} = props;
     const cartItemsMap = useSelector(cartSelectors.selectCartItemsMap);
     const currencyType = useSelector(settingsSelectors.selectCurrencyType);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     return <>
+        <PizzaModal
+            classNameContainer={styles.orderFormModalContainer}
+            classNameModal={styles.orderFormModal}
+        >
+            <PizzaOrderForm onSubmit={(data) => console.log(data)}/>
+        </PizzaModal>
         <h2 className={styles.title}>Your order</h2>
         <PizzaCartList totalPrice={totalPrice} currencyType={currencyType} cartItemsMap={cartItemsMap}/>
     </>
